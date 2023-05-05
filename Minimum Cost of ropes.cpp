f2f9171711acde65ca7c6688351d1905450a850f
pradnya kondubhairi <91897968+pradnyakondubhairi02@users.noreply.github.com>
2023-05-06 00:08:58 +0530

    Create Minimum Cost of ropes.cpp

---
+++ Minimum Cost of ropes.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+       
+        // priority_queue<long long , greater<long long>>pq;
+         priority_queue<long long , vector<long long>, greater<long long> >pq;
+        
+        for(int i=0;i<n;i++){
+            pq.push(arr[i]);
+        }
+        
+        long long int ans=0;
+        
+        while(pq.size()>1){
+           long long temp=pq.top();
+           pq.pop();
+           
+           temp=temp+pq.top();
+           pq.pop();
+           
+           pq.push(temp);
+           
+           ans=ans+temp;
+        }
+        
+       
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
